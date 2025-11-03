f90fe6dc40c9c95965870d22d013404a7d30bd25
Irshad Ahmed <88178320+iaschowrai@users.noreply.github.com>
2025-11-02 18:57:35 -0700

    Change title from 'Resume' to 'Work Experience'

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -8,7 +8,7 @@ const Resume = () => {
   return (
     <article className="resume active" data-page="resume">
       <header>
-        <h2 className="h2 article-title">Resume</h2>
+        <h2 className="h2 article-title">Work Experience</h2>
       </header>
       <Experience />
       <Education />
